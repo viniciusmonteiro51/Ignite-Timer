a4bf6d3c60d5fca893bdba912ad3c804cd10c9cf
Vinicius51 <contato.viniciusdanielmonteiro@gmail.com>
2025-02-12 13:03:12 -0400

    avancando no projeto

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -11,25 +11,63 @@ import {
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import * as zod from 'zod';
+import { useState } from "react";
 
-const newCycleFormSchema = zod.object({
+const newCycleFormValidationSchema = zod.object({
     task: zod.string().min(1, "Informe a tarefa"),
-    minutesAmount: zod.number().min(5).max(60)
+    minutesAmount: zod.number()
+    .min(5, "O ciclo precisa ser de no minímo 05 minutos.")
+    .max(60, "O ciclo precisa ser de no máximo 60 minutos.")
 });
+
+type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>
+
+interface Cycle {
+    id: string;
+    task: string;
+    minutesAmount: number;
+}
 
 export default function Home() {
 
-    const { register, handleSubmit, watch, formState } = useForm({
-        resolver: zodResolver(newCycleFormSchema)
-    });
+    const [cycles, setCycles] = useState<Cycle[]>([]);
+    const [activeCycleId, setActiveCycleId] = useState<string | null>(null);
+    const [ammountSecondsPasssed, setAmmountSecondsPassed] = useState(0);
 
-    function handleCreateNewCycle(data: any) {
-        console.log(data)
-    }
+    const { register, handleSubmit, watch, reset } = useForm<NewCycleFormData>({
+        resolver: zodResolver(newCycleFormValidationSchema),
+        defaultValues: {
+            task: "",
+            minutesAmount: 0
+        }
+    })
 
+    function handleCreateNewCycle(data: NewCycleFormData) {
+        const id = String(new Date().getTime());
+        const newCycle: Cycle = {
+            id: id,
+            task: data.task,
+            minutesAmount: data.minutesAmount
+        };
+
+        setCycles((state) => [...state, newCycle]);
+        setActiveCycleId(id);
+        reset();
+    }  
+
+    const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId);
+
+    const totalSeconds = activeCycle ? activeCycle.minutesAmount * 60 : 0;
+    const currentSeconds = activeCycle ? totalSeconds - ammountSecondsPasssed : 0;
+
+    const minutesAmount = Math.floor(currentSeconds / 60);
+    const secondsAmount = currentSeconds % 60;
+
+    const minutes = String(minutesAmount).padStart(2, '0')
+    const seconds = String(secondsAmount).padStart(2, '0')
     const task = watch('task');
     const isSbumitDisabled = !task;
-
+    
     return (
         <HomeContainer>
             <form onSubmit={handleSubmit(handleCreateNewCycle)} action="">
@@ -59,7 +97,7 @@ export default function Home() {
                         placeholder="00"
                         step={5}
                         min={5}
-                        max={60}
+                        // max={60}
                         {...register('minutesAmount', {valueAsNumber: true})}
                     />
 
@@ -67,11 +105,11 @@ export default function Home() {
                 </FormContainer>
 
                 <CountDownContainer>
-                    <span>0</span>
-                    <span>0</span>
+                    <span>{minutes[0]}</span>
+                    <span>{minutes[1]}</span>
                     <Separator>:</Separator>
-                    <span>0</span>
-                    <span>0</span>
+                    <span>{seconds[0]}</span>
+                    <span>{seconds[1]}</span>
                 </CountDownContainer>
 
                 <StartCountDownButton disabled={isSbumitDisabled} type="submit">
